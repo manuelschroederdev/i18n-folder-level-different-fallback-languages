12a48aa4f41ec373e7cac830c937205c81da6db2
Manuel Schröder <manuel.schroeder@storyblok.com>
2023-09-07 12:36:29 +0200

    style update

--- components/LanguageInfo.vue
+++ components/LanguageInfo.vue
@@ -4,7 +4,7 @@ defineProps({ language: String, fallback_lang: String })
 
 <template>
   <header
-    class="rounded-md shadow-lg container max-w-xl py-3 px-6 top-3 left-1/2 -translate-x-1/2 fixed border-[#50b0ae] border bg-opacity-50 text-sm"
+    class="rounded-md shadow-lg container max-w-xl py-3 px-6 top-3 left-1/2 -translate-x-1/2 fixed bg-white backdrop-blur-lg border-[#50b0ae] border bg-opacity-50 text-sm"
   >
     You're viewing the <span class="text-[#50b0ae] font-bold">{{ language }}</span> version of this story. The defined
     fallback language is <span class="text-[#50b0ae] font-bold">{{ fallback_lang }}</span
